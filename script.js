df6833fbe1e42d6e6d3ef55f65e85cc027e67a43
Viczdera <vichizsoft@yahoo.com>
2023-07-30 23:08:36 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+function playRound(playerSelection,computerSelection){
+return playerSelection
+}
+
+const playerSelection=prompt('Rock,paper,scissors')
+const computerSelection="paper"
+console.log(playRound(playerSelection,computerSelection))
